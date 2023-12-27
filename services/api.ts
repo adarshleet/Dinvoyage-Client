4a61aca8950a4eed1585953111365958035d6a53
Adarsh <adarshravi011@gmail.com>
2023-12-27 21:15:27 +0530

    base url problem

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const Api = axios.create({baseURL:process.env.NEXT_PUBLIC_BASE_URL,withCredentials:true});
+const Api = axios.create({baseURL:'https://dinevoyage.online',withCredentials:true});
 
 export default Api;
